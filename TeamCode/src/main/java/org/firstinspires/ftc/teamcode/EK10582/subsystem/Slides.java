29357f5a5784c7e5b1755b6399a4901f50baa6df
Jack <jackjkliu50@gmail.com>
2024-09-27 22:35:45 -0400

    slide test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EK10582/subsystem/Slides.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.EK10582.subsystem;
+
+public class Slides {
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EK10582/test/SlidesTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EK10582/test/SlidesTester.java
@@ -1,23 +1,27 @@
 package org.firstinspires.ftc.teamcode.EK10582.test;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.EK10582.EKLinear;
 import org.firstinspires.ftc.teamcode.EK10582.subsystem.Robot;
-@Disabled
-@TeleOp(name="Motor Tester")
-public class MotorTester extends EKLinear {
+
+@TeleOp(name="Slides Tester")
+public class SlidesTester extends EKLinear {
     @Override
     public void runOpMode() {
         waitForStart();
-        DcMotor testMotor = hardwareMap.get(DcMotor.class, "Test Motor");
+        DcMotor sm1 = hardwareMap.get(DcMotor.class, "sm1");
+        DcMotor sm2 = hardwareMap.get(DcMotor.class, "sm2");
         while(opModeIsActive()) {
-            testMotor.setPower(gamepad1.right_trigger*.5);
-            testMotor.setPower(-gamepad1.left_trigger*.5);
+            sm1.setPower(gamepad1.right_trigger*.5);
+            sm2.setPower(gamepad1.right_trigger*.5);
+            sm1.setPower(-gamepad1.left_trigger*.5);
+            sm2.setPower(-gamepad1.left_trigger*.5);
             telemetry.addData("motor speed: ", gamepad1.right_trigger);
             telemetry.update();
         }
     }
+
 }
