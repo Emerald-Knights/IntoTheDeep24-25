2839a28c90276b9c251827bb4b4103071028ab2c
Shining <shinypup06@gmail.com>
2024-10-05 22:34:45 -0400

    meepmeepwip

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutonSim.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutonSim.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
@@ -13,16 +14,19 @@ public class AutonSim {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(24, 66, Math.toRadians(-90)))
+                        .lineToLinearHeading(new Pose2d(60,60, Math.toRadians(-135)))
+                        .splineToSplineHeading(new Pose2d(36,26,Math.toRadians(0)),Math.toRadians(-90))
+                        .forward(5)
+                        .back(1)
+                        .splineToSplineHeading(new Pose2d(60,60,Math.toRadians(-135)),Math.toRadians(90))
+//                        .turn(Math.toRadians(90))
+//                        .lineToLinearHeading(new Pose2d(60,60, Math.toRadians(-135)))
+//                        .lineToLinearHeading(new Pose2d(58 ,38, Math.toRadians(-90)))
+//                        .lineToLinearHeading(new Pose2d(60,60, Math.toRadians(-135)))
+
                         .build());
+
 
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
