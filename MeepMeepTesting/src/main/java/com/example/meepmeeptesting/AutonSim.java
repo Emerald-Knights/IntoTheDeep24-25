ec9106fdc5dc82c2cafdedc92fa2db5baeb36738
Jason <jasonqi890@gmail.com>
2024-10-05 22:47:29 -0400

    MeepMeep code

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutonSim.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutonSim.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
@@ -13,15 +14,18 @@ public class AutonSim {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(24, 66, Math.toRadians(-90)))
+                        .lineToLinearHeading(new Pose2d(60,60, Math.toRadians(-135)))
+                        .splineToSplineHeading(new Pose2d(36,24,Math.toRadians(0)),Math.toRadians(-90))
+                        .forward(2)
+                        .splineToSplineHeading(new Pose2d(60,60, Math.toRadians(25)), Math.toRadians(60))
+                        .splineToSplineHeading(new Pose2d(48,24,Math.toRadians(0)),Math.toRadians(-90))
+                        .forward(2)
+                        .splineToSplineHeading(new Pose2d(60,60, Math.toRadians(25)), Math.toRadians(60))
+                        .splineToSplineHeading(new Pose2d(60,24,Math.toRadians(0)),Math.toRadians(-90))
+                        .forward(2)
+                        .splineToSplineHeading(new Pose2d(60,60, Math.toRadians(25)), Math.toRadians(60))
+
                         .build());
 
 
